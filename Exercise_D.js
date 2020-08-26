a1c8e264698d9700c96cc15f797a4768ff25b0f4
B Leung <leungcheukkei@gmail.com>
2020-08-26 23:11:18 +0800

    initial commit

--- Exercise_D.js
+++ Exercise_D.js
@@ -6,15 +6,29 @@
 /*  ====================== */
 /** Write a function that expects a number as a parameter. 
  * If the number that is entered is less than 0, equal to 0, or not a number, 
- * the function should return the string ‘ERROR’. If the number that is entered 
- * is greater than or equal to 1000000 it should simply return the number. 
+ * the function should return the string ‘ERROR’. 
+ * 
+ * If the number that is entered is greater than or equal to 1,000,000 it should simply return the number. 
+ * 
  * Otherwise it should multiply the number by 10 however many times it takes 
- * to get a number that is greater than or equal to 1000000 and return that number. 
+ * to get a number that is greater than or equal to 1,000,000 and return that number. 
+ * 
  * Consider which loop you will need to use to do this.
+ * 
 Use a combination of conditionals and loops to create your answer for this question. */
 
 function multiplyNumber(number) {
-  // CODE HERE
+  if (isNaN(number) || number <= 0){
+    return ('ERROR');
+  } else if (number >= 1000000){
+    return number;
+  } else {
+    while (number < 1000000);{
+      number *= 10;
+    }
+    return number;
 }
+} 
 
-console.log(multiplyNumber(10));
+console.log(multiplyNumber("100000"));
+
