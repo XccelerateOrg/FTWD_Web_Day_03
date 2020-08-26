e17d2b67a485a5a2e24ed9e1998a6d678976ced5
ckc213 <javierckc213@gmail.com>
2020-08-26 22:00:27 +0800

    day3

--- Exercise_D.js
+++ Exercise_D.js
@@ -15,6 +15,17 @@ Use a combination of conditionals and loops to create your answer for this quest
 
 function multiplyNumber(number) {
   // CODE HERE
+  if (number <= 0 || isNaN(number)) {
+    return "ERROR"
+  } else if (number >= 1000000) {
+    return number;
+  } 
+
+while (number < 1000000) {
+   number = number * 10;
 }
 
-console.log(multiplyNumber(10));
+return number;
+}
+
+console.log(multiplyNumber(2000));
