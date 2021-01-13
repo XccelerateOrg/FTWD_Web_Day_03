303674f285242554d6c5bded1445d3cfb59d4022
Jeffrey Cheung <jeffcheung@Jeffreys-MacBook-Air.local>
2021-01-13 17:54:27 +0800

    day 03

--- Exercise_C.js
+++ Exercise_C.js
@@ -13,6 +13,22 @@ If the number is divisible by 3 and 5 it should log Hong Kong.
 See README.md if you need help running your code.
 */
 
-for (let i = 1; i <= 30; i++) {
-  // CODE HERE
+for (i = 0; ++i < 31; console.log(i % 5 ? x || i : x + "Kong"))
+  x = i % 3 ? "" : "Hong";
+
+// OR
+
+function loopyLoop(number) {
+  var array = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push("Hong Kong");
+    } else if (i % 3 === 0 && i % 5) {
+      array.push("Hong");
+    } else if (i % 5 === 0 && i % 3) {
+      array.push("Kong");
+    } else array.push(i);
+  }
+  return array;
 }
+loopyLoop(30);
--- Exercise_E.js
+++ Exercise_E.js
@@ -21,20 +21,29 @@ The output should be always a string with one word, all lowercase.
 Maya won’t forget to thank you at the end of her article :)*/
 
 var KEYS = {
-  "6": "a",
-  "1": "b",
-  "7": "d",
-  "4": "e",
-  "3": "i",
-  "2": "l",
-  "9": "m",
-  "8": "n",
-  "0": "o",
-  "5": "t",
+  6: "a",
+  1: "b",
+  7: "d",
+  4: "e",
+  3: "i",
+  2: "l",
+  9: "m",
+  8: "n",
+  0: "o",
+  5: "t",
 };
 
 function maya(number) {
   // CODE HERE
-}
+  if (number > 100 && number < 999999) {
+    return ("" + number)
+      .split ("") 
+      .map((x) => KEYS[x]) 
+      .join ("");
+   
+    } else {
+      return "ERROR!"
+    }
+  }
 
 console.log(maya(423));
