f236c049f0465d98a7fd3cc4d4fa4f6fe30cccd1
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2021-08-17 17:14:06 +0800

    Jasmine D3 Exercises

--- extra.js
+++ extra.js
@@ -9,11 +9,20 @@
  * The calculator function should handle these operators: +, -, /, * (Addition, Subtraction, Division and Multiplication)
  * The numbers should always be positive */
 
-function calculator(operator, a, b) {
-  switch (
-    operator
+ function calculator(operator, a, b) {
+  switch (operator) {
     // CODE HERE
-  ) {
+    case "+":
+      return a + b;
+    
+      case "-":
+        return a - b;
+      
+      case "*":
+        return a * b;
+
+      case '/':
+        return a / b;     
   }
 }
 
