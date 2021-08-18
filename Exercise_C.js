5a8e04724199958b6cd908d46eed73141655f006
Karma Isabell <karmaisabelle90@gmail.com>
2021-08-18 17:15:11 +0800

    finished homework

--- Exercise_C.js
+++ Exercise_C.js
@@ -13,6 +13,18 @@ If the number is divisible by 3 and 5 it should log Hong Kong.
 See README.md if you need help running your code.
 */
 
-for (let i = 1; i <= 30; i++) {
-  // CODE HERE
+let i = 1;
+
+function fizbuzz(number) {
+  for (let i = 1; i <= 30; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("Hong Kong");
+    } else if (i % 5 == 0) {
+      console.log("Kong");
+    } else if (i % 3 == 0) {
+      console.log("Hong");
+    } else console.log(i);
+  }
+  return Number;
 }
+fizbuzz(30);
