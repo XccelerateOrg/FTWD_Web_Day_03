4f03fac1daae99a5111acd90eb62cec792838728
Claire-Lai <laiclairetk@gmail.com>
2022-01-12 23:02:08 +0800

    Done F

--- Exercise_F.js
+++ Exercise_F.js
@@ -10,6 +10,15 @@
 
 function occurrence(string, targetChar) {
   // CODE HERE
+  // const array = string.split("");
+  let times =0;
+  let newString = string.toLowerCase();
+  for (let i=0; i < newString.length; i++){
+    if (newString[i]===targetChar){
+      times+=1
+    }
+  }
+  return times
 }
 
 console.log(occurrence("baa", "a"));
