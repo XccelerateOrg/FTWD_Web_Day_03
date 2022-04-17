ce5e4256aadef4a968fbea1666e6c28529b52acb
Jeff Widjaja <jvwidjaja@gmail.com>
2022-04-17 12:11:58 +0800

    JeffWidjajaDay3

--- Exercise_F.js
+++ Exercise_F.js
@@ -8,8 +8,16 @@
  * The function will count the number of times that character appears in the string.
  * The count is case insensitive. */
 
-function occurrence(string, targetChar) {
+ function occurrence(string, targetChar) {
   // CODE HERE
+  let count = 0;
+  for(let i of string){
+      i
+      if(i === targetChar){
+          count++
+      }
+  }
+  return count;
 }
 
 console.log(occurrence("baa", "a"));
